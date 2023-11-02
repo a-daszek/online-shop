33c4a77c54035d88c90f79a8ca76613af0ba84c2
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-11-02 18:49:08 +0100

    added admin users

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,12 +6,28 @@ const validation = require("../util/validation");
 const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res) { //renders sign up form
-    res.render("customer/auth/signup");
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData){
+        sessionData = {
+            email: "",
+            password: "",
+            confirmEmail: "",
+            fullname: "",
+            street: "",
+            postal: "",
+            city: "",
+
+        };
+    }
+
+    res.render("customer/auth/signup", { inputData: sessionData });
 }
 
 async function signup(req, res, next) {  //takes the user data from form
     const enteredData = {
         email: req.body.email,
+        confirmEmail: req.body["confirm-email"],
         password: req.body.password,
         fullname: req.body.fullname,
         street: req.body.street,
@@ -70,7 +86,16 @@ async function signup(req, res, next) {  //takes the user data from form
 }
 
 function getLogin(req, res) {
-    res.render("customer/auth/login")
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData){
+        sessionData = {
+            email: "",
+            password: "",
+        };
+    }
+
+    res.render("customer/auth/login", { inputData: sessionData });
 }
 
 async function login(req, res, next) { //see methods in models/user.model.js
@@ -84,7 +109,7 @@ async function login(req, res, next) { //see methods in models/user.model.js
     }
 
     const sessionErrorData = {
-        errorMessage: "Invalid credentials, please check your email and/or password ",
+        errorMessage: "Invalid credentials, please check your email and/or password.",
         email: user.email,
         password: user.password
     };
