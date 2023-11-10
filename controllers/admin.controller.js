ea2b8fb0a0ac0ca565d495ce4c824f6ada1508fb
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-11-10 22:28:35 +0100

    added logic for updating products

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,54 +1,70 @@
 const Product = require("../models/product.model");
 
 async function getProducts(req, res, next) {
-    try {
-        const products = await Product.findAll();
-        res.render("admin/products/all-products", { products: products }); //we are handling products by keyname "products" (the name is up to us) into that template
-    } catch (error) {
-        next(error);
-        return;
-    }
+  try {
+    const products = await Product.findAll();
+    res.render("admin/products/all-products", { products: products }); //we are handling products by keyname "products" (the name is up to us) into that template
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
-function getNewProduct(req, res){
-    res.render("admin/products/new-product");
+function getNewProduct(req, res) {
+  res.render("admin/products/new-product");
 }
 
-async function createNewProduct(req, res, next){
-    // console.log(req.body);
-    // console.log(req.file);
-    const product = new Product({
-        ...req.body,
-        image: req.file.filename
-    });
+async function createNewProduct(req, res, next) {
+  // console.log(req.body);
+  // console.log(req.file);
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
 
-    try { //express does not handle error that are caused by promises on its own
-        await product.save();
-    } catch (error) {
-        next(error);
-        return;
-    }
+  try { //express does not handle error that are caused by promises on its own
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
-    res.redirect("/admin/products");
+  res.redirect("/admin/products");
 }
 
-async function getUpdateProduct (req, res, next){
-    try {
-        const product = await Product.findById(req.params.id);
-        res.render("admin/products/update-product", { product: product }); //product after "try" is passed into this template (it's on the left)
-    } catch (error){
-        next(error);
-    }
+async function getUpdateProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.render('admin/products/update-product', { product: product });
+  } catch (error) {
+    next(error);
+  }
 }
 
-function updateProduct (){
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id,
+  });
 
+  if (req.file) {
+    product.replaceImage(req.file.filename);
+  }
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.redirect('/admin/products');
 }
 
 module.exports = {
-    getProducts: getProducts,
-    getNewProduct: getNewProduct,
-    createNewProduct: createNewProduct,
-    getUpdateProduct: getUpdateProduct,
-    updateProduct: updateProduct
-}
+  getProducts: getProducts,
+  getNewProduct: getNewProduct,
+  createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct,
+};
--- models/product.model.js
+++ models/product.model.js
@@ -3,55 +3,85 @@ const mongodb = require("mongodb");
 const db = require("../data/database");
 
 class Product {
-    constructor(productData){
-        this.title = productData.title;
-        this.summary = productData.summary;
-        this.price = +productData.price; //"+" forces a conversion to number
-        this.description = productData.description;
-        this.image = productData.image; //the name of the img file
-        this.imagePath = `product-data/images/${productData.image}`;
-        this.imageUrl = `/products/assets/images/${productData.image}`;
-        if (productData._id) {
-            this.id = productData._id.toString();
-        }
+  constructor(productData) {
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = +productData.price; //"+" forces a conversion to number
+    this.description = productData.description;
+    this.image = productData.image; //the name of the img file
+    this.updateImageData();
+    if (productData._id) {
+      this.id = productData._id.toString();
+    }
+  }
+
+  static async findById(productId) {
+    let prodId;
+    try {
+      prodId = new mongodb.ObjectId(productId);
+    } catch (error) {
+      error.code = 404;
+      throw error;
+    }
+    const product = await db
+      .getDb()
+      .collection('products')
+      .findOne({ _id: prodId });
+
+    if (!product) {
+      const error = new Error('Could not find product with provided id.');
+      error.code = 404;
+      throw error;
     }
 
-    static async findById(productId) {
-        let prodId;
-        try {
-            prodId = new mongodb.ObjectId(productId);
-        } catch (error){
-            error.code = 404;
-            throw error;
+    return new Product(product);
+  }
+
+  static async findAll() {//with static method we don't need to instantiate the class first, we don't need to create an object based 
+    // on the class in order to use static methods
+    const products = await db.getDb().collection("products").find().toArray();
+
+    return products.map(function (productDocument) { //transorming an array full of product documents into an array of products objects that are based on our class
+      return new Product(productDocument); //we do that to have imagePath and imageUrl
+    });
+  }
+
+  updateImageData() {
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
+  }
+
+  async save() {
+    const productData = {
+      title: this.title,
+      summary: this.summary,
+      price: this.price,
+      description: this.description,
+      image: this.image,
+    };
+
+    if (this.id) {
+      const productId = new mongodb.ObjectId(this.id);
+
+      if (!this.image) {
+        delete productData.image;
+      }
+
+      await db.getDb().collection('products').updateOne(
+        { _id: productId },
+        {
+          $set: productData,
         }
-        const product = await db.getDb().collection("products").findOne({_id: prodId });
-        if (!product){
-            const error = new Error("Couldn't find product with provided id.");
-            error.code = 404;
-            throw error;
-        }
-        return product;
+      );
+    } else {
+      await db.getDb().collection('products').insertOne(productData);
     }
+  }
 
-    static async findAll() {//with static method we don't need to instantiate the class first, we don't need to create an object based 
-        // on the class in order to use static methods
-        const products = await db.getDb().collection("products").find().toArray();
-
-        return products.map(function (productDocument) { //transorming an array full of product documents into an array of products objects that are based on our class
-            return new Product(productDocument); //we do that to have imagePath and imageUrl
-        });
-    } 
-
-    async save() {
-        const productData = {
-            title: this.title,
-            summary: this.summary,
-            price: this.price,
-            description: this.description,
-            image: this.image
-        };
-        await db.getDb().collection("products").insertOne(productData);
-    }
+  replaceImage(newImage) {
+    this.image = newImage;
+    this.updateImageData();
+  }
 }
 
 module.exports = Product;
