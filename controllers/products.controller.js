806df1f922db620bd079ffd410da79ce5f7e5900
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-12-23 13:44:02 +0100

    product details output

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -3,12 +3,22 @@ const Product = require("../models/product.model");
 async function getAllProducts (req, res, next){
     try {
         const products = await Product.findAll(); //those products are the second ones a line below
-        res.render("customer/products/all-products", { products: products });
+        res.render('customer/products/all-products', { products: products });
     } catch (error) {
-        next(error);
+      next(error);
     }
-}
-
-module.exports = {
+  }
+  
+  async function getProductDetails(req, res, next) {
+    try {
+      const product = await Product.findById(req.params.id);
+      res.render('customer/products/product-details', { product: product });
+    } catch (error) {
+      next(error);
+    }
+  }
+  
+  module.exports = {
     getAllProducts: getAllProducts,
-};
+    getProductDetails: getProductDetails
+  };
