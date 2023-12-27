eae1a0ec7e9e3f9ea1613a3495558cebe14724a6
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-12-27 20:17:20 +0100

    cart update

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,60 +1,56 @@
 class Cart {
-    constructor(items = [], totalQuantity = 0, totalPrice = 0){ //adding a default value
-        this.items = items;
-        this.totalQuantity = totalQuantity;
-        this.totalPrice =totalPrice;
+    constructor(items = [], totalQuantity = 0, totalPrice = 0) {
+      this.items = items;
+      this.totalQuantity = totalQuantity;
+      this.totalPrice = totalPrice;
     }
-// this.items.push(product); this alone would work but - adding the same item multiple times would push multiple same items in the array. We want to do something different -
-// we should have one product where we will keep track of the quantity of this product. Pushing is okay if a product is not a part of this items array yet.
-
-    addItem(product){
-        const cartItem = {
-            product: product,
-            quantity: 1,
-            totalPrice: product.price
-        };
-
-        for (let i = 0; i < this.items.length; i++){
-            const item = this.items[i];
-            if (item.product.id === product.id){
-                cartItem.quantity = item.quantity + 1;
-                cartItem.totalPrice = item.totalPrice + product.price;
-                this.items[i] = cartItem;
-
-                this.totalQuantity++;
-                this.totalPrice += product.price;
-                return;
-            }
-            
+  
+    addItem(product) {
+      const cartItem = {
+        product: product,
+        quantity: 1,
+        totalPrice: product.price,
+      };
+  
+      for (let i = 0; i < this.items.length; i++) {
+        const item = this.items[i];
+        if (item.product.id === product.id) {
+          cartItem.quantity = item.quantity + 1;
+          cartItem.totalPrice = item.totalPrice + product.price;
+          this.items[i] = cartItem;
+  
+          this.totalQuantity++;
+          this.totalPrice += product.price;
+          return;
         }
-        this.items.push(cartItem)
-        this.totalQuantity++;
-        this.totalPrice += product.price;
-       
+      }
+  
+      this.items.push(cartItem);
+      this.totalQuantity++;
+      this.totalPrice += product.price;
     }
-
-    updateItem(productId, newQuantity){ // hmm
-        for (let i = 0; i < this.items.length; i++){
-            const item = this.items[i];
-            if (item.product.id === productId && newQuantity > 0){
-                const cartItem = {...item};
-                const quantityChange = newQuantity - item.quantity;
-                cartItem.quantity = newQuantity;
-                cartItem.totalPrice = newQuantity * product.price;
-                this.items[i] = cartItem;
-
-                this.totalQuantity = this.totalQuantity + quantityChange;
-                this.totalPrice += quantityChange * product.price;
-                return { updatedItemPrice: cartItem.totalPrice };
-            } else if (item.product.id === productId && newQuantity <= 0){
-                this.items.splice(i, 1);
-                this.totalQuantity = this.totalQuantity - item.quantity;
-                this.totalPrice -= item.totalPrice;
-                return { updatedItemPrice: 0 };
-            }
-            
+  
+    updateItem(productId, newQuantity) {
+      for (let i = 0; i < this.items.length; i++) {
+        const item = this.items[i];
+        if (item.product.id === productId && newQuantity > 0) {
+          const cartItem = { ...item };
+          const quantityChange = newQuantity - item.quantity;
+          cartItem.quantity = newQuantity;
+          cartItem.totalPrice = newQuantity * item.product.price;
+          this.items[i] = cartItem;
+  
+          this.totalQuantity = this.totalQuantity + quantityChange;
+          this.totalPrice += quantityChange * item.product.price;
+          return { updatedItemPrice: cartItem.totalPrice };
+        } else if (item.product.id === productId && newQuantity <= 0) {
+          this.items.splice(i, 1);
+          this.totalQuantity = this.totalQuantity - item.quantity;
+          this.totalPrice -= item.totalPrice;
+          return { updatedItemPrice: 0 };
         }
+      }
     }
-}
-
-module.exports = Cart;
+  }
+  
+  module.exports = Cart;
--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -7,31 +7,32 @@ async function addToCart(){
 
     let response;
     try {
-        response = await fetch("/cart/items", {
-            method: "POST",
-            body: JSON.stringify({
-                productId: productId,
-                _csrf: csrfToken
-            }),
-            headers: { // ?
-                "Content-Type": "application/json"
-            }
-        });
-        
+      response = await fetch("/cart/items", {
+        method: "POST",
+        body: JSON.stringify({
+          productId: productId,
+          _csrf: csrfToken
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    
     } catch (error) {
-        alert("Something went wrong.");
-        return
+      alert("Something went wrong!");
+      return;
     }
-
-    if (!response.ok){
-        alert("Something went wrong.");
-        return
+    
+    if (!response.ok) {
+      alert("Something went wrong!");
+      return;
     }
-
+  
     const responseData = await response.json();
-
+  
     const newTotalQuantity = responseData.newTotalItems;
+  
     cartBadgeElement.textContent = newTotalQuantity;
-}
-
-addToCartButtonElement.addEventListener("click", addToCart);
+  }
+  
+  addToCartButtonElement.addEventListener("click", addToCart);
